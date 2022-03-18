36df668be3a9bb2bbe42e88b4d78ef5f82490214
truc-nguyenminh <truc.nguyenminh93@gmail.com>
2022-03-18 13:56:49 +0700

    config templates & build models

--- devforum_d/settings.py
+++ devforum_d/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'devforum_d.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'frontend/templates/',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
