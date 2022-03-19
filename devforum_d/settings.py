da28375e30181b5a609f73cd22b41729acd0db74
truc-nguyenminh <truc.nguyenminh93@gmail.com>
2022-03-19 22:13:36 +0700

    config media files & add Image field into Project model

--- devforum_d/settings.py
+++ devforum_d/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8&lx$&hmk3z=z5$w*hl1i#z896f$9=mqja7dw_qw4_fdu=fj#^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,11 +123,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'frontend/static/root/'
+
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'frontend/static/',
 ]
+
+MEDIA_ROOT = BASE_DIR / 'frontend/static/images/'
+
+MEDIA_URL = 'images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
