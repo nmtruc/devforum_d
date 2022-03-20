67110ca85dd531a440fbabe704b07dc6af7af411
truc-nguyenminh <truc.nguyenminh93@gmail.com>
2022-03-20 14:46:38 +0700

    change django app's name & add new Users app

--- devforum_d/urls.py
+++ devforum_d/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('backend.urls')),
+    path('', include('projects.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
